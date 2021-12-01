0b32cc98d080626dda85e4fad3c6f0d5edd10608
benorio <b3norio@gmail.com>
2021-11-30 22:47:34 -0300

    Curso-logica-condicional-fluxos-Java

---
+++ LogicaCondicionalControleDeFluxosEmJava/src/blocos/Teste.java
@@ -0,0 +1,29 @@
+package blocos;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Blocos: É um grupo de 0 ou mais códigos quais trabalham em conjunto para executar
+		 * uma operação
+		 * 
+		 * Tipos:
+		 * 
+		 * 1- Locais: dentro de métodos
+		 * 2- Estáticos: dentro de classes
+		 * 3- Instância: dentro de classes
+		 * 
+		 * Tudo que estiver dentro de chaves { } é um bloco
+		 * 
+		 * Exemplo:
+		 * 
+		 * if (autorizado) {
+		 * 		carregarPerfil
+		 * 		DirecionarPáginaPrincipal
+		 * } esse bloco tem duas linhas de execução
+		*/	
+	}
+
+}
